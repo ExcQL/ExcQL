48d6ca4d3c70a4d216367ca3b60ed507c3889a6c
Nattie C <47332368+nattiechan@users.noreply.github.com>
2023-03-06 13:27:03 -0800

    Add ability to specify column to table mapping
    Re-designed the info panel to separate upload file
    and submit process to accommodate the intermediate
    step of requesting users to input column to table
    mapping:
    
    TODO:
    - Add "delete" button to delete column/table mapping
    row
    - Upon clicking `upload`, check to make sure we
    have at least one column/table pair before proceding

--- client/components/Info Panel/InfoPanel.js
+++ client/components/Info Panel/InfoPanel.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { RiFolderUploadFill } from 'react-icons/ri';
 
 import './InfoPanel.css';
 
+const createMappingTemplate = (id) => {
+  return { mappingId: id, fileColumn: '', tableName: '' }
+};
+
+const initialMappingState = {
+  mapping: [...Array(3).keys()].map(num => createMappingTemplate(num)),
+  nextMappingId: 3,
+};
+
 const InfoPanel = () => {
+  const [mappingState, setMappingState] = useState(initialMappingState);
+
   const uploadExcelHandler = async (e) => {
     e.preventDefault();
 
@@ -22,32 +33,87 @@ const InfoPanel = () => {
     console.log(data);
   };
 
+  const handleUploadFile = (event) => {
+    const fileNameElement = document.getElementById('file-name');
+    fileNameElement.innerHTML = event.target.files[0].name;
+  }
+
+  const handleInputChange = (event) => {
+    const newMapping = [...mappingState.mapping];
+    newMapping.forEach(record => {
+      if (record.mappingId === Number(event.target.id)) {
+        record[event.target.name] = event.target.value;
+      }
+    })
+    setMappingState({ ...mappingState, mapping: newMapping });
+  }
+
+  const handleMoreInputClick = (event) => {
+    event.preventDefault();
+    const newId = mappingState.nextMappingId + 1;
+    setMappingState({ ...mappingState, mapping: [...mappingState.mapping, createMappingTemplate(newId)], nextMappingId: newId });
+  }
+
+  console.log(mappingState);
   return (
     <section className="info-panel">
       <h1 className="info-panel__main-heading">ExcQL</h1>
       <p className="info-panel__description">
-        Lorem ipsum, dolor sit amet consectetur adipi sicing elit. Volupta tibus
-        aliquid pariatur enim vitae accusantium qua erat cum! Nisi totam
-        deserunt labore, aliquam repellat repudiandae perspiciatis explicabo
-        pariatur ducimus commo di nihil velit! Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Dolor natus volup tate beatae fugit quidem
-        accusamus provident nostrum sequi quas!
+        Translating excel files to SQL script is as easy as 1, 2, 3!
       </p>
       <form
         action="/test"
         className="info-panel__upload-form"
         onSubmit={uploadExcelHandler}
       >
-        <label className="upload-form--container">
-          <input type="file" id="files" name="files" />
+        <span className="info-panel__steps">
+          Step 1: Upload your file
+        </span>
+        <div className="upload-form--container">
+          <input type="file" id="files" name="files" accept=".xls, .xlsx" onChange={handleUploadFile} />
           <RiFolderUploadFill className="info-panel__upload-icon" />
-        </label>
-        <button type="submit" className="info-panel__upload-btn">
-          Upload!
-        </button>
+          <span id='file-name'></span>
+        </div>
+        <span className="info-panel__steps">
+          Step 2: Declare file columns to SQL tables mapping
+        </span>
+        <p className="info-panel__instructions">
+          How would you like your information to split into separate tables?
+          Open your file in Excel. Put in the column letter that corresponds to the start of a table
+          under "Column Letter", and then input a table name of your choice under "Table Name" in the same row.
+        </p>
+        <div className="mapping-input--container">
+          <header>
+            <span>Column Letter</span>
+            <span>Table Name</span>
+          </header>
+          {mappingState.mapping.map(record =>
+            <MappingInput key={record.mappingId} id={record.mappingId} handleInputChange={handleInputChange} />
+          )
+          }
+          <button id='more-inputs' onClick={handleMoreInputClick}>Add More</button>
+        </div>
+        <span className="info-panel__steps">
+          Step 3: Upload the file and watch magic happen!
+        </span>
+        <div className="upload-form--container">
+          <button type="submit" className="info-panel__upload-btn">
+            Upload!
+          </button>
+        </div>
       </form>
-    </section>
+    </section >
   );
 };
+
+const MappingInput = (props) => {
+  const { id, handleInputChange } = props;
+  return (
+    <div className='input-container'>
+      <input id={id} name='fileColumn' type='text' onChange={handleInputChange} />
+      <input id={id} name='tableName' type='text' onChange={handleInputChange} />
+    </div>
+  );
+}
 
 export default InfoPanel;
